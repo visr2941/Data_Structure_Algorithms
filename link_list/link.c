d69fc0d5863d46b30241d250a91d309da938c614
Vishal Srivastav <visr2941@colorado.edu>
2017-02-18 20:12:00 -0700

    reverse-list func added and modified delete-list func

--- link_list/link.c
+++ link_list/link.c
@@ -48,13 +48,19 @@ void print_list(Node ** head)
 	printf("\n");
 }
 
-void delete_node(Node ** head, uint8_t pos)
+int8_t delete_node(Node ** head, uint8_t pos)
 {
 	uint8_t i=1;
 	Node * temp1 = *head;
 	Node * temp = NULL;
+	printf("(trying to delete node no:%d)\n", pos);
 	while(i!=pos)
 	{
+		if(temp1==NULL)
+			{
+				printf("error: node not in the list!\n\n");
+				return -1;
+			}
 		temp = temp1;
 		temp1 = temp1->nxtptr;
 		i++;
@@ -64,6 +70,23 @@ void delete_node(Node ** head, uint8_t pos)
 	else
 		temp->nxtptr=temp1->nxtptr;
 	free(temp1);
+	printf("deleted node no:%d!\n", pos);
+	return 0;
+}
+
+void reverse_list(Node ** head)
+{
+	Node * temp = *head;
+	Node * temp1 = NULL;
+	Node * temp2 = NULL;
+	while((temp)!=NULL)
+	{
+		temp1 = temp -> nxtptr;
+		temp -> nxtptr = temp2;
+		temp2=temp;
+		temp = temp1;
+	}
+	*head = temp2;
 }
 
 main()
@@ -71,13 +94,15 @@ main()
     	Node ** head = (Node **)malloc(sizeof(*head));
     	*head = NULL;
 	insert_node(head, 1, -34);
-	insert_node(head, 2, 7);
 	insert_node(head, 3, -7);
 	insert_node(head, 4, 8);
 	insert_node(head, 5, 100);
 	insert_node(head, 6, -800);
 	print_list(head);
-	delete_node(head, 6);
-	delete_node(head, 5);
+	delete_node(head, 9);
+	delete_node(head, 2);
+	delete_node(head, 4);
+	delete_node(head, 10);
+	reverse_list(head);
 	print_list(head);
 }
