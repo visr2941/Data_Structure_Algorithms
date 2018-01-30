da7f0456d476e160c2e35243bb918a72b26bdf8d
visr2941 <visr2941@colorado.edu>
2018-01-30 16:50:36 -0600

    added findlowest and delete function

--- BST.c
+++ BST.c
@@ -26,17 +26,55 @@ void insert(bst_node** root, int data)
     return;
 }
 
-bst_node * search(bst_node ** root, int data)
+
+bst_node** search(bst_node ** root, int data)
 {
     if(*root == NULL || (*root)->data == data)
-        return *root;
+        return root;
     else if((*root)->data > data)
         return search(&(*root)->left, data);
     else if((*root)->data < data)
        return search(&(*root)->right, data);
 }
 
-//searching without recursion
+
+bst_node ** findLowest(bst_node ** root)
+{
+    if((*root)->left != NULL)
+        root = findLowest(&((*root)->left));
+    else
+        return root;
+}
+
+
+void delete (bst_node ** root, int data)
+{
+    bst_node ** nodeDeleted = search(root, data);
+    if((*nodeDeleted)->left == NULL && (*nodeDeleted)->right == NULL)
+    {   
+        free(*nodeDeleted);
+        *nodeDeleted = NULL;
+    }
+    else if ((*nodeDeleted)->left == NULL)
+    {
+        *nodeDeleted = (*nodeDeleted)-> right;
+    }
+        else if ((*nodeDeleted)->right == NULL)
+    {
+        *nodeDeleted = (*nodeDeleted)-> left;
+    }
+    else
+    {
+       bst_node ** temp = findLowest(&((*nodeDeleted)->right));
+       (*nodeDeleted)->data = (*temp)->data;
+        free(*temp);
+        *temp = NULL;
+    }
+        
+}
+
+
+
 /*bst_node * search(bst_node ** root, int data)
 {
     bst_node * rtnPtr = *root;
@@ -65,21 +103,31 @@ int main()
     insert(&root, 7);
     insert(&root, 15);
     insert(&root, 11);
-    insert(&root, 4);
-    insert(&root, 5);
-    insert(&root, 15);
+    insert(&root, 8);
+    insert(&root, 9);
+    insert(&root, 16);
     
-    bst_node * searchfound = search(&root, 11);
+    bst_node ** searchfound = search(&root, 15);
     
-    if(searchfound) {
-        printf("Hello World %d\n", searchfound->data);
-        printf("Hello World %d\n", searchfound->right->data);    
-        
+    if(*searchfound) {
+        printf("Hello World %d\n", (*searchfound)->data);
+        printf("Hello World %d\n", (*searchfound)->right->data);    
     }
     else
         printf("Not found");    
+    
+    delete (&root, 15);
+    
+    searchfound = search(&root, 15);
+    
+    if(*searchfound) {
+        printf("Hello World %d\n", (*searchfound)->data);
+        printf("Hello World %d\n", (*searchfound)->right->data);    
+        
+    }
+    else
+        printf("Not found");      
+    
 
     return 0;
 }
-
-
