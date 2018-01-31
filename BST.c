4f07b89e9cd4adb70b17ee31d011d64b5a8bf625
visr2941 <visr2941@colorado.edu>
2018-01-31 10:55:33 -0600

    added comments and a function to initialize BST

--- BST.c
+++ BST.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 
+/* structure of a bst node*/
 typedef struct bst_node { 
     int data;
     struct bst_node * left;
@@ -8,8 +9,10 @@ typedef struct bst_node {
 
 } bst_node;
 
+/* to insert root node, or any leaf in the bst */
 void insert(bst_node** root, int data)
 {
+    // if the BST is empty, add the root node
     if(*root == NULL)
     {
         *root = (bst_node *) malloc(sizeof(bst_node));
@@ -18,6 +21,8 @@ void insert(bst_node** root, int data)
         (*root)->right = NULL;
         return;
     }
+    /* if root node is already present, compare the data 
+       and insert on left if smaller and on right if larger */ 
     else if(data < (*root)->data)
         insert(&((*root)->left), data);
     else if(data >= (*root)->data)
@@ -26,10 +31,15 @@ void insert(bst_node** root, int data)
     return;
 }
 
-
+/* search a node with the data input */
 bst_node** search(bst_node ** root, int data)
 {
-    if(*root == NULL || (*root)->data == data)
+    // if the BST is empty, return NULL
+    if(*root == NULL || root == NULL)
+        return NULL;
+    /* BST not empty, compare the data and call the function
+       recursively until data found */
+    else if((*root)->data == data)
         return root;
     else if((*root)->data > data)
         return search(&(*root)->left, data);
@@ -37,32 +47,54 @@ bst_node** search(bst_node ** root, int data)
        return search(&(*root)->right, data);
 }
 
-
+/* fucntion to find the lowest in BST */
 bst_node ** findLowest(bst_node ** root)
 {
-    if((*root)->left != NULL)
+    // passing invalid pointers or BST empty
+    if(*root == NULL || root == NULL)
+        return NULL;
+    // find the lowest recursively
+    else if((*root)->left != NULL)
         root = findLowest(&((*root)->left));
     else
         return root;
 }
 
-
+/* function to delete the node with the given data */
 void delete (bst_node ** root, int data)
 {
+    // passing invalid pointers or BST empty
+    if(root == NULL || *root == NULL)
+        return;
+    
+    // look for the node to be deleted
     bst_node ** nodeDeleted = search(root, data);
+    
+    // if the node is leaf node, simply delete it
     if((*nodeDeleted)->left == NULL && (*nodeDeleted)->right == NULL)
     {   
         free(*nodeDeleted);
         *nodeDeleted = NULL;
     }
+    /* if the node has a sub-tree on right (nothing on left)
+    ** delete the node and attach the right sub-tree to the main 
+    ** tree. Samething will happen if nothing is present on left
+    ** but there is sub-tree on right
+    */
     else if ((*nodeDeleted)->left == NULL)
     {
+        free(*nodeDeleted);
         *nodeDeleted = (*nodeDeleted)-> right;
     }
-        else if ((*nodeDeleted)->right == NULL)
+    else if ((*nodeDeleted)->right == NULL)
     {
+        free(*nodeDeleted);
         *nodeDeleted = (*nodeDeleted)-> left;
     }
+    /* if the node to be deleted has both right and left sub-tree,
+    go to the right sub-tree and find the lowest value node. Replace
+    data of the node to be deleted with this lowest value, and delete
+    the lowest value node */
     else
     {
        bst_node ** temp = findLowest(&((*nodeDeleted)->right));
@@ -72,12 +104,24 @@ void delete (bst_node ** root, int data)
         
 }
 
+/* function to initialize a BST */
+void initialize_bst(bst_node** root, int data)
+{
+    if(*root != NULL)
+    {
+        *root = (bst_node *) malloc(sizeof(bst_node));
+        (*root)->data = data;
+        (*root)->left = NULL;
+        (*root)->right = NULL;
+    }
+    else
+        printf("BST is already initialized");
+}
 
-
+/* search code without recursion */
 /*bst_node * search(bst_node ** root, int data)
 {
     bst_node * rtnPtr = *root;
-
     while(rtnPtr != NULL)
     {
         if(rtnPtr->data != data)
@@ -98,32 +142,39 @@ void delete (bst_node ** root, int data)
 int main()
 {
     bst_node* root = NULL;
+   
+    initialize_bst(&root, 10);
     insert(&root, 10);
     insert(&root, 7);
     insert(&root, 15);
     insert(&root, 11);
     insert(&root, 8);
     insert(&root, 9);
-    insert(&root, 16);
+    insert(&root, 18);
+    insert(&root, 17);
     
-    bst_node ** searchfound = search(&root, 15);
-    
-    if(*searchfound) {
-        printf("Hello World %d\n", (*searchfound)->data);
-        printf("Hello World %d\n", (*searchfound)->right->data);    
-    }
+    bst_node** searchfound = search(&root, 15);
+    if(searchfound) 
+        if(*searchfound) {
+            printf("Hello World %d\n", (*searchfound)->data);
+            printf("Hello World %d\n", (*searchfound)->right->data);   
+        }
+        else
+            printf("Not found");
     else
-        printf("Not found");    
-    
+        printf("Not found");  
+        
     delete (&root, 15);
     
-    searchfound = search(&root, 15);
+    searchfound = search(&root, 100);
     
-    if(*searchfound) {
-        printf("Hello World %d\n", (*searchfound)->data);
-        printf("Hello World %d\n", (*searchfound)->right->data);    
-        
-    }
+    if(searchfound) 
+        if(*searchfound) {
+            printf("Hello World %d\n", (*searchfound)->data);
+            printf("Hello World %d\n", (*searchfound)->right->data);   
+        }
+        else
+            printf("Not found");
     else
         printf("Not found");      
     
