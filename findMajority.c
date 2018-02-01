22d1b1051bf6dd795fc4f916e8f1eb3a6b88e55b
visr2941 <visr2941@colorado.edu>
2018-02-01 10:57:29 -0600

    Adding function to find the majority element in array

---
+++ findMajority.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+
+/* this function will find the majority number in the array i.e count
+** of that number is more than 50% else it will return -1. This algorithm
+** works on the fact that the majority in main array will be definitely a
+** majority in either of the half array. So, we check recursively to find
+** the majority. Complexity is O(nlogn).
+*/
+int findMajority(int arr[], int start, int end)
+{
+    int maj, mid = (start + end)/2, i, maj1, maj2, cnt1=0, cnt2=0;
+    
+    //if array has only one number, that is the majority
+    if(start >= end)
+        return arr[start];
+    else 
+    {
+        // recursive call to check the majority in 1st half 
+        // and then second.
+        maj1 = findMajority(arr, start, mid);
+        maj2 = findMajority(arr, mid+1, end);
+
+        // scan the array and count the value of maj2 & maj2
+        for(i = 0; i <= end; i++)
+        {
+            if(arr[i]==maj1)
+                cnt1++;
+            else if(arr[i]==maj2) 
+                cnt2++;
+        }
+
+        // based on the count, return the majority
+        if(cnt2 > cnt1 && cnt2 > (end+1)/2)
+            return maj2;
+        else if(cnt1 > cnt2 && cnt1 > (end+1)/2)
+            return maj1;
+        else
+            return -1;
+    }
+    
+}
+
+
+
+int main()
+{
+    int A[] = {12, 14,12,12,13,14,12, 14, 14};
+	int N = sizeof(A)/sizeof(A[0]);
+	
+	for(int i = 0; i< N; i++)
+	    printf("%d ", A[i]);
+    printf("\n");
+    
+    int ans = findMajority(A, 0, N-1);
+    
+    printf("majority is %d\n", ans);
+
+    return 0;
+}
