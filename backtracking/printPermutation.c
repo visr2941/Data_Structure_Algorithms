59951287d5ccd46e1ebbdd8b1bd1d5f22f32d6ee
visr2941 <visr2941@colorado.edu>
2018-04-25 23:49:27 -0500

    Create printPermutation.c

---
+++ backtracking/printPermutation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "backtracking.h"
+
+int main()
+{
+    char str[] = "vishal";
+    PrintPermutation(str, 6);
+
+    return 0;
+}
