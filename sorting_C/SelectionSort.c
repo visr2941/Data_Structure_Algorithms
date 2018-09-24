75dbb488dae6cd31a8fb719aba80b2dd19063846
visr2941 <visr2941@colorado.edu>
2018-09-23 21:47:52 -0500

    Update SelectionSort.c

--- sorting_C/SelectionSort.c
+++ sorting_C/SelectionSort.c
@@ -7,38 +7,38 @@
 */
 void SelectionSort(int arr[], int n)
 {
-  int min, swapTemp;
-	for (int i = 0; i < n-1; i++)
-	{
-		min = i;
-		for(int j = i+1; j < n; j++)
-		{
-			if(arr[min] > arr[j])
-				min = j;
-		}
+    int min, swapTemp;
+    for (int i = 0; i < n-1; i++)
+    {
+        min = i;
+        for(int j = i+1; j < n; j++)
+        {
+            if(arr[min] > arr[j])
+                min = j;
+        }
 		
-		swapTemp = arr[i];
-		arr[i] = arr[min];
-		arr[min] = swapTemp;
+        swapTemp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = swapTemp;
 	}
 }
 
 /* Function to print an array */
 void printArray(int arr[], int size)
 {
-	int i;
-	for (i=0; i < size; i++)
-  printf("%d ", arr[i]);
-	printf("\n");
+    int i;
+    for (i=0; i < size; i++)
+    printf("%d ", arr[i]);
+    printf("\n");
 }
 
 // Driver program to test above functions
 int main()
 {
-	int arr[] = {64, 25, 12, 22, 11};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	SelectionSort(arr, n);
-	printf("Sorted array: \n");
-	printArray(arr, n);
-	return 0;
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    SelectionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
 }
