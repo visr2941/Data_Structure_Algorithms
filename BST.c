da41344b4232a12c6e47ad413b6725d42e9f88a8
visr2941 <visr2941@colorado.edu>
2018-11-12 22:16:20 -0600

    Update BST.c

--- BST.c
+++ BST.c
@@ -205,6 +205,35 @@ void PrintPreOrder(bst_node ** root)
     PrintPreOrder(&(*root)->right);
 }
 
+static int findHeight (Node * root)
+{
+    if(root == NULL) return 0;
+    
+    int lh = findHeight(root->left);
+    int rh = findHeight(root->right);
+    
+    return (lh > rh ? lh+1 : rh+1);
+}
+
+static void levelOrderutil(Node *root, int level)
+{
+    if(root == NULL) return;
+    
+    if(level == 1) printf("%d ", root->data);
+    else if(level > 1)
+    {
+        levelOrderutil(root->left, level-1);
+        levelOrderutil(root->right, level-1);
+    }
+}
+
+void PrintLevelOrder(Node* root){
+  //Write your code here
+    int height = findHeight(root);
+    for(int i = 1; i <= height; i++)
+        levelOrderutil(root, i);
+}
+
 int main()
 {
     bst_node* root = NULL;
