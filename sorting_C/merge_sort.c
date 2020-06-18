38e0838142322fdf9bcc9268bfb0c900373b3c42
visr2941 <visr2941@colorado.edu>
2020-06-17 19:59:50 -0700

    updated merge sort with optimization

--- sorting_C/merge_sort.c
+++ sorting_C/merge_sort.c
@@ -10,81 +10,66 @@
      printf("\n");
  }
 
- /* this function merge two sorted arrays into a single sorted
- ** array. */
- void merge(int arr[], int n1, int n2, int n3)
- {
-     int i=n1, j=n2+1, k=0;
+/* this function merge two sorted arrays into a single sorted
+** array. */
+void MergeArray(int left[], int n1, int right[], int n2)
+{
+    int i=0, j=0;
 
-     int size = n3-n1+1;
-     int* res = (int *) malloc(size*sizeof(int));
-     
-     // Merge the temp arrays back into arr[l..r]
-     while (i <= n2 && j <= n3)
-     {
-         if (arr[i] >= arr[j])
-         {
-             res[k] = arr[j];
-             j++;
-         }
-         else
-         {
-             res[k] = arr[i];
-             i++;
-         }
-         k++;
-     }
-  
-     // Copy the remaining elements of left[], if there are any
-     while (i <= n2)
-     {
-         res[k] = arr[i];
-         i++;
-         k++;
-     }
-     
-     // Copy the remaining elements of right[], if there are any
-     while (j <= n3)
-     {
-         res[k] = arr[j];
-         j++;
-         k++;
-     }
-     
-     i = n1;
-     j = 0;
-     
-     // copy the sorted array back to the main array
-     while(i<=n3)
-     {
-         arr[i] = res[j];
-         i++;
-         j++;
-     }
- }
+    int* merge_result = (int *) malloc((n1+n2)*sizeof(int));
+    
+    // Merge the temp arrays back into arr[l..r]
+    while (i < n1 || j < n2)
+    {
+        if (i < n1 && (j >= n2 || left[i] <= right[j]))
+        {
+            merge_result[i+j] = left[i];
+            i++;
+        }
+        else
+        {
+            merge_result[i+j] = right[j];
+            j++;
+        }
+    }
+    
+    // copy the data back to original array
+    for (int i = 0; i < n1; i++)
+    {
+        left[i] = merge_result[i];
+    }
+    // copy the data back to original array
+    for (int i = n1, j = 0; i < (n1+n2); i++, j++)
+    {
+        right[j] = merge_result[i];
+    }
+    
+    // free heap memory
+    free(merge_result);
+}
 
- /* this sorting method is a O(nlogn) time complexity sorting
- ** algorithm. the main array is divided into two equal (unequal)
- ** halves and those are sorted first and then merged to get a
- ** new sorted array. the main function calls itself recursively
- ** to sort the divided arrays and then merge to get a single
- ** sorted array. */
- void mergeSort(int arr[], int l, int m)
- {
-     if (l >= m)
-       return;
+/* this sorting method is a O(nlogn) time complexity sorting
+** algorithm. the main array is divided into two equal (unequal)
+** halves and those are sorted first and then merged to get a
+** new sorted array. the main function calls itself recursively
+** to sort the divided arrays and then merge to get a single
+** sorted array. */
+void MergeSortArray(int arr[], int size)
+{
+    if (size <= 1)
+      return;
 
-     int mid = l + (m-l)/2;
+    int mid = size/2;
 
-     // calling mergeSort() function recursively to sort
-     // the new smaller arrays.
-     mergeSort(arr, l, mid);
-     mergeSort(arr, mid+1, m);
-     
-     // after two halves are sorted, merge them to get a
-     // single merged sorted array
-     merge(arr, l, mid, m);
- }
+    // calling mergeSort() function recursively to sort
+    // the new smaller arrays.
+    MergeSortArray(arr, mid);
+    MergeSortArray(arr+mid, size-mid);
+    
+    // after two halves are sorted, merge them to get a
+    // single merged sorted array
+    MergeArray(arr, mid, arr+mid, size-mid);
+}
 
 /* Driver program to test above functions */
 int main(int argc, const char * argv[])
@@ -95,7 +80,7 @@ int main(int argc, const char * argv[])
     printf("Given array is \n");
     printArray(arr, arr_size);
  
-    mergeSort(arr, 0, arr_size-1);
+    MergeSortArray(arr, arr_size);
  
     printf("\nSorted array is \n");
     printArray(arr, arr_size);
